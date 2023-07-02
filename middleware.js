dedabeacdaefc2d7d988f675805bce51b3fb93ae
hyejin <hj10150@gmail.com>
2023-07-02 23:53:23 +0900

    최종

--- middleware.js
+++ middleware.js
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
   const session = await getToken({ req: request });
 
-  if (request.nextUrl.pathname.startsWith("/write")) {
-    if (session === null) {
-      return NextResponse.redirect("http://localhost:3000/api/auth/signin");
-    }
-  }
+  // if (request.nextUrl.pathname.startsWith("/write")) {
+  //   if (session === null) {
+  //     return NextResponse.redirect("http://localhost:3000/api/auth/signin");
+  //   }
+  // }
 
   if (request.nextUrl.pathname.startsWith("/register")) {
     if (request.cookies.has("visited") === false) {
